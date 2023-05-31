11ca410761073d2e1dcdf705cbcd3c6d41175597
Enrico <e.weissenrieder94@gmail.com>
2023-05-31 14:31:20 +0200

    Add Exercise Functions

---
+++ src/main/java/Test.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args){
+
+inputSum();
+
+
+    }
+    public static void inputSum(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geben Sie einen Text ein und drücken Sie ENTER:");
+        int input1 = scanner.nextInt();
+        int input2 = scanner.nextInt();
+
+        System.out.println("\n" + "Summe: " +(input1+input2));
+    }
+
+
+    public static int returnSum(int num1, int num2){
+        return num1+num2;
+    }
+    public static String sumString(int num1, int num2){
+        return "Summe: "+ (num1+num2);
+    }
+
+    public static int fak(int value){
+        int result = value;
+        for(int i=value-1; i>=1; i--){
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static int fak2(int value){
+        int result = value;
+        int i = value -1;
+        while(i>=1){
+            result = result * i;
+            i--;
+        }
+        return result;
+    }
+
+}
